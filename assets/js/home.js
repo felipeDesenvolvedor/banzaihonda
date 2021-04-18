51d51b91f9eac4f373c30b94e9bcf6e8eacd4db5
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-04-18 06:53:15 -0300

    DadosA e Slider

---
+++ assets/js/home.js
@@ -0,0 +1,1 @@
+"use strict";$(".homeDadosA__conteudo:last-child .homeDadosA__list").slick({infinite:!0,slidesToShow:3,slidesToScroll:3,dots:!0,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]});
